871fe0451520f09c95fee6ab235f76b000ca47d0
Patricio Torres <patriciotorres1992@hotmail.com>
2023-02-23 23:39:33 -0300

    creando

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// ARRAYS
+const productos = [
+  { nombre: "Pantalla", precio: 35000 },
+  { nombre: "PlayStation", precio: 40000 },
+  { nombre: "Teclado", precio: 12000 },
+  { nombre: "Celular", precio: 25000 },
+];
+let carrito = [];
+let total = 0;
+// let
